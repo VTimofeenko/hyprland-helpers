886bd7f8fa88ce7bd4f317b78066426eee6e8c36
Vladimir Timofeenko <id@vtimofeenko.com>
2026-02-28 11:00:41 -0800

    chore: remove unused args

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
 
-      perSystem = { config, self', inputs', pkgs, system, ... }:
+      perSystem = { config, pkgs, ... }:
         let
           craneLib = crane.mkLib pkgs;
           version = "0.1.0";
